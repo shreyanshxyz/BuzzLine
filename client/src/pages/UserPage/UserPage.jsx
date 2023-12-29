ab0e266260443f6a4e3b4e7c6e550b1ed8a28426
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-29 13:39:02 +0530

    feat: create UserPage.jsx

---
+++ client/src/pages/UserPage/UserPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserHeader from "../../components/Header/UserHeader";
+
+function UserPage() {
+  return (
+    <>
+      <UserHeader />
+    </>
+  );
+}
+
+export default UserPage;
