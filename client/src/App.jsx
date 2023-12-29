351a1c462ee903089fb1c8ef13764d1454b5d11a
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-29 13:03:30 +0530

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { Button } from "@chakra-ui/button";
+import { Container } from "@chakra-ui/react";
 function App() {
-  return <div>App</div>;
+  return (
+    <Container maxW="720px">
+      <Button>Hello</Button>
+    </Container>
+  );
 }
 
 export default App;
