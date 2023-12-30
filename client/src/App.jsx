7a6ebdc4283d72b8cde70f9e7336a62ae284bb93
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-30 23:10:27 +0530

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { Button } from "@chakra-ui/button";
 import { Container } from "@chakra-ui/react";
+import HomePage from "./pages/HomePage/HomePage";
 import UserPage from "./pages/UserPage/UserPage";
 import PostPage from "./pages/PostPage/PostPage";
 import Header from "./components/Header/Header";
@@ -11,6 +12,7 @@ function App() {
     <Container maxW="740px">
       <Header />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route
           path="/:username"
           element={
