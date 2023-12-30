5ce2d58e19013eb0d22d100b274073d6dee63d2a
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-30 19:16:05 +0530

    update Comment.jsx

--- client/src/components/Comments/Comment.jsx
+++ client/src/components/Comments/Comment.jsx
@@ -1,39 +1,35 @@
 import { Avatar, Divider, Flex, Text } from "@chakra-ui/react";
 
-const Comment = () =>
-  // { reply, lastReply }
-  {
-    return (
-      <>
-        <Flex gap={4} py={2} my={2} w={"full"}>
-          <Avatar
-            borderRadius={"2px"}
-            src={"/gojo_icon.jpg"}
-            // {reply.userProfilePic}
-            size={"sm"}
-          />
-          <Flex gap={1} w={"full"} flexDirection={"column"}>
-            <Flex
-              w={"full"}
-              justifyContent={"space-between"}
-              alignItems={"center"}
-            >
-              <Text fontSize="sm" fontWeight="bold">
-                {/* {reply.username} */}
-                username
-              </Text>
-            </Flex>
-            <Text>
-              {/* {reply.text} */}
-              Comment
+const Comment = ({ userProfilePic, username, text, lastReply }) => {
+  return (
+    <>
+      <Flex gap={4} py={2} my={2} w={"full"}>
+        <Avatar
+          borderRadius={"2px"}
+          // reply.
+          src={userProfilePic}
+          size={"sm"}
+        />
+        <Flex gap={1} w={"full"} flexDirection={"column"}>
+          <Flex
+            w={"full"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Text fontSize="sm" fontWeight="bold">
+              {/* reply. */}
+              {username}
             </Text>
           </Flex>
+          <Text>
+            {/* reply. */}
+            {text}
+          </Text>
         </Flex>
-        {/* {!lastReply ?  */}
-        <Divider />
-        {/* : null} */}
-      </>
-    );
-  };
+      </Flex>
+      {!lastReply ? <Divider /> : null}
+    </>
+  );
+};
 
 export default Comment;
