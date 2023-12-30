35536a366104d50be458f2dcea308c63f29e5d73
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-30 14:13:04 +0530

    update UserPage.jsx

--- client/src/pages/UserPage/UserPage.jsx
+++ client/src/pages/UserPage/UserPage.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import UserHeader from "../../components/Header/UserHeader";
+import UserPost from "../../components/Post/UserPost";
 
 function UserPage() {
   return (
     <>
       <UserHeader />
+      <UserPost
+        likes={1200}
+        replies={481}
+        postImg={"./havertz.jpg"}
+        postTitle={"Kai Havertz"}
+      />
+      <UserPost
+        likes={1200}
+        replies={481}
+        postImg={"./odegaard.jpg"}
+        postTitle={"Did you know?"}
+      />
+      <UserPost
+        likes={1200}
+        replies={481}
+        postImg={"./havertz.jpg"}
+        postTitle={"Heluuuu"}
+      />
+      <UserPost likes={1200} replies={481} postTitle={"Heluuuu"} />
     </>
   );
 }
