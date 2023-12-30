3a57473afaf60e2737c746d79467bde671842160
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-30 21:36:57 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
-// import postRoutes from "./routes/postRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 // import messageRoutes from "./routes/messageRoutes.js";
 // import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/users", userRoutes);
-// app.use("/api/posts", postRoutes);
+app.use("/api/posts", postRoutes);
 // app.use("/api/messages", messageRoutes);
 
 server.listen(PORT, () =>
