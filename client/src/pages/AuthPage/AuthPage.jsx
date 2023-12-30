bfec53a099a7f709882de31a16a5af36f0e676a0
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-31 00:02:39 +0530

    update AuthPage.jsx

--- client/src/pages/AuthPage/AuthPage.jsx
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -1,20 +1,12 @@
 import { useRecoilValue } from "recoil";
 import LoginCard from "../../components/Auth/LoginCard";
-// import SignupCard from "../../components/Auth/SignupCard";
-// import authScreenAtom from "../atoms/authAtom";
+import SignupCard from "../../components/Auth/SignupCard";
+import authScreenAtom from "../../atoms/authAtom";
 
 const AuthPage = () => {
-  // const authScreenState = useRecoilValue(authScreenAtom);
+  const authScreenState = useRecoilValue(authScreenAtom);
 
-  return (
-    <>
-      {/* {authScreenState === "login" ?  */}
-      <LoginCard />
-      {/* :  */}
-      {/* <SignupCard /> */}
-      {/* } */}
-    </>
-  );
+  return <>{authScreenState === "login" ? <LoginCard /> : <SignupCard />}</>;
 };
 
 export default AuthPage;
