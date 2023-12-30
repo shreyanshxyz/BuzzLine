65477739eb89561d745894a4d10214c00a84cffc
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-31 00:09:42 +0530

    update LogoutButton.jsx

--- client/src/components/Auth/LogoutButton.jsx
+++ client/src/components/Auth/LogoutButton.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@chakra-ui/button";
 import { useSetRecoilState } from "recoil";
-import userAtom from "../atoms/userAtom";
-import useShowToast from "../hooks/useShowToast";
+import userAtom from "../../atoms/userAtom";
+import useShowToast from "../../hooks/useShowToast";
 import { FiLogOut } from "react-icons/fi";
 
 const LogoutButton = () => {
