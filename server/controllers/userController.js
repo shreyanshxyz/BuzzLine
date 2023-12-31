16d5ff5c3270d72faf08681fdac941c275dbbf5e
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-31 23:52:01 +0530

    update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,11 +15,10 @@ const getUserProfile = async (req, res) => {
 
     // query is userId
     if (mongoose.Types.ObjectId.isValid(query)) {
-      // user = await User.findOne({ _id: query })
-      //   .select("-password")
-      //   .select("-updatedAt");
-      // }
-      // else {
+      user = await User.findOne({ _id: query })
+        .select("-password")
+        .select("-updatedAt");
+    } else {
       // query is username
       user = await User.findOne({ username: query })
         .select("-password")
