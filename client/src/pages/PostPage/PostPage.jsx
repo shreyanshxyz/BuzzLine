32d023dc86a7c8a26bd02365b1cd9700933314d8
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-31 22:11:33 +0530

    update Actions.jsx and PostPage.jsx

--- client/src/pages/PostPage/PostPage.jsx
+++ client/src/pages/PostPage/PostPage.jsx
@@ -8,29 +8,86 @@ import {
   Spinner,
   Text,
 } from "@chakra-ui/react";
+import Actions from "../../components/Actions/Actions";
 import { useEffect } from "react";
+import Comment from "../../components/Comments/Comment";
+import useGetUserProfile from "../../hooks/useGetUserProfile";
+import useShowToast from "../../hooks/useShowToast";
 import { useNavigate, useParams } from "react-router-dom";
 import { formatDistanceToNow } from "date-fns";
 import { useRecoilState, useRecoilValue } from "recoil";
+import userAtom from "../../atoms/userAtom";
 import { DeleteIcon } from "@chakra-ui/icons";
-import Actions from "../../components/Actions/Actions";
-import Comment from "../../components/Comments/Comment";
+import postsAtom from "../../atoms/postsAtom";
 
-function PostPage() {
+const PostPage = () => {
+  const { user, loading } = useGetUserProfile();
+  const [posts, setPosts] = useRecoilState(postsAtom);
+  const showToast = useShowToast();
+  const { pid } = useParams();
+  const currentUser = useRecoilValue(userAtom);
+  const navigate = useNavigate();
+
+  const currentPost = posts[0];
+
+  useEffect(() => {
+    const getPost = async () => {
+      setPosts([]);
+      try {
+        const res = await fetch(`/api/posts/${pid}`);
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setPosts([data]);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      }
+    };
+    getPost();
+  }, [showToast, pid, setPosts]);
+
+  const handleDeletePost = async () => {
+    try {
+      if (!window.confirm("Are you sure you want to delete this post?")) return;
+
+      const res = await fetch(`/api/posts/${currentPost._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
+      showToast("Success", "Post deleted", "success");
+      navigate(`/${user.username}`);
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    }
+  };
+
+  if (!user && loading) {
+    return (
+      <Flex justifyContent={"center"}>
+        <Spinner size={"xl"} />
+      </Flex>
+    );
+  }
+
+  if (!currentPost) return null;
+  console.log("currentPost", currentPost);
+
   return (
     <>
       <Flex>
         <Flex w={"full"} alignItems={"center"} gap={3}>
-          <Avatar
-            src={"/gojo_icon.jpg"}
-            size={"md"}
-            borderRadius={"2px"}
-            name="Shreyansh Bhadoria"
-          />
+          <Avatar src={user.profilePic} size={"md"} name="Mark Zuckerberg" />
           <Flex>
             <Text fontSize={"sm"} fontWeight={"bold"}>
-              shreyanshxyz
+              {user.username}
             </Text>
+            <Image src="/verified.png" w="4" h={4} ml={4} />
           </Flex>
         </Flex>
         <Flex gap={4} alignItems={"center"}>
@@ -40,51 +97,48 @@ function PostPage() {
             textAlign={"right"}
             color={"gray.light"}
           >
-            30 December 2023
-            {/* {formatDistanceToNow(new Date(currentPost.createdAt))} ago */}
+            {formatDistanceToNow(new Date(currentPost.createdAt))} ago
           </Text>
 
-          {/* {currentUser?._id === user._id && ( */}
-          {/* <DeleteIcon size={20} cursor={"pointer"} onClick={handleDeletePost} /> */}
-          {/* )} */}
+          {currentUser?._id === user._id && (
+            <DeleteIcon
+              size={20}
+              cursor={"pointer"}
+              onClick={handleDeletePost}
+            />
+          )}
         </Flex>
       </Flex>
 
-      <Text my={3}>Sample text for the post.{/* {currentPost.text} */}</Text>
+      <Text my={3}>{currentPost.text}</Text>
 
-      {/* {currentPost.img && ( */}
-      <Box
-        borderRadius={6}
-        overflow={"hidden"}
-        border={"1px solid"}
-        borderColor={"gray.light"}
-      >
-        <Image
-          src={"/havertz.jpg"}
-          // {currentPost.img}
-          w={"full"}
-        />
-      </Box>
-      {/* )} */}
+      {currentPost.img && (
+        <Box
+          borderRadius={6}
+          overflow={"hidden"}
+          border={"1px solid"}
+          borderColor={"gray.light"}
+        >
+          <Image src={currentPost.img} w={"full"} />
+        </Box>
+      )}
 
       <Flex gap={3} my={3}>
-        <Actions
-        // post={currentPost}
-        />
+        <Actions post={currentPost} />
       </Flex>
 
       <Divider my={4} />
 
-      {/* <Flex justifyContent={"space-between"}>
+      <Flex justifyContent={"space-between"}>
         <Flex gap={2} alignItems={"center"}>
           <Text fontSize={"2xl"}>👋</Text>
           <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
         </Flex>
         <Button>Get</Button>
-      </Flex> */}
+      </Flex>
 
-      <Divider bg={"gray"} my={4} />
-      {/* {currentPost.replies.map((reply) => (
+      <Divider my={4} />
+      {currentPost.replies.map((reply) => (
         <Comment
           key={reply._id}
           reply={reply}
@@ -93,27 +147,9 @@ function PostPage() {
             currentPost.replies[currentPost.replies.length - 1]._id
           }
         />
-      ))} */}
-      <Comment
-        userProfilePic={"/gojo_icon.jpg"}
-        username={"shreyanshx"}
-        text={"Nice Man!"}
-        lastReply={false}
-      />
-      <Comment
-        userProfilePic={"/gojo_icon.jpg"}
-        username={"shreyanshxy"}
-        text={"Nice One Man!"}
-        lastReply={false}
-      />
-      <Comment
-        userProfilePic={"/gojo_icon.jpg"}
-        username={"shreyanshxyy"}
-        text={"Nice Two Man!"}
-        lastReply={true}
-      />
+      ))}
     </>
   );
-}
+};
 
 export default PostPage;
