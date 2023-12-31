05cf6663032d1275157553003b255483dceba2a4
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-31 19:13:33 +0530

    update UserPage.jsx

--- client/src/pages/UserPage/UserPage.jsx
+++ client/src/pages/UserPage/UserPage.jsx
@@ -1,32 +1,66 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import UserHeader from "../../components/Header/UserHeader";
-import UserPost from "../../components/Post/UserPost";
+import { useParams } from "react-router-dom";
+import useShowToast from "../../hooks/useShowToast";
+import { Flex, Spinner } from "@chakra-ui/react";
+import Post from "../../components/Post/Post";
+import useGetUserProfile from "../../hooks/useGetUserProfile";
+import { useRecoilState } from "recoil";
+import postsAtom from "../../atoms/postsAtom";
 
-function UserPage() {
+const UserPage = () => {
+  const { user, loading } = useGetUserProfile();
+  const { username } = useParams();
+  const showToast = useShowToast();
+  const [posts, setPosts] = useRecoilState(postsAtom);
+  const [fetchingPosts, setFetchingPosts] = useState(true);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      if (!user) return;
+      setFetchingPosts(true);
+      try {
+        const res = await fetch(`/api/posts/user/${username}`);
+        const data = await res.json();
+        console.log(data);
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+        setPosts([]);
+      } finally {
+        setFetchingPosts(false);
+      }
+    };
+
+    getPosts();
+  }, [username, showToast, setPosts, user]);
+
+  if (!user && loading) {
+    return (
+      <Flex justifyContent={"center"}>
+        <Spinner size={"xl"} />
+      </Flex>
+    );
+  }
+
+  if (!user && !loading) return <h1>User not found</h1>;
+
   return (
     <>
-      <UserHeader />
-      <UserPost
-        likes={3002}
-        replies={866}
-        postImg={"./havertz.jpg"}
-        postTitle={"Kai Havertz"}
-      />
-      <UserPost
-        likes={1200}
-        replies={481}
-        postImg={"./odegaard.jpg"}
-        postTitle={"Did you know?"}
-      />
-      <UserPost
-        likes={1341}
-        replies={391}
-        postImg={"./aurora.jpg"}
-        postTitle={"Northern Lights - Norway"}
-      />
-      <UserPost likes={862} replies={123} postTitle={"Heluuuu"} />
+      <UserHeader user={user} />
+
+      {!fetchingPosts && posts.length === 0 && <h1>User has not posts.</h1>}
+      {fetchingPosts && (
+        <Flex justifyContent={"center"} my={12}>
+          <Spinner size={"xl"} />
+        </Flex>
+      )}
+
+      {posts.map((post) => (
+        <Post key={post._id} post={post} postedBy={post.postedBy} />
+      ))}
     </>
   );
-}
+};
 
 export default UserPage;
