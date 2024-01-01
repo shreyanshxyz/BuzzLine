d16e3ffdb218a1aedbeb79943cdbb9bdef4feb0f
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-01 14:27:04 +0530

    Changed CORS origin

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // Get rid of the CORS error
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://buzzline-backend151571.onrender.com",
         changeOrigin: true,
         secure: false,
       },
